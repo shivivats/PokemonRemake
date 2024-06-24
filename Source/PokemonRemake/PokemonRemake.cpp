6bb8e928a48293778bb244898fad8441ab0c9f1b
Shivi Vats <shivivats7@gmail.com>
2024-06-24 21:35:51 +0200

    initial commit with pokemon assets and basic bp made

---
+++ Source/PokemonRemake/PokemonRemake.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PokemonRemake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PokemonRemake, "PokemonRemake" );
+ 
