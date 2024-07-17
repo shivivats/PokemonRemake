df56c743f890e6df5b136f3a2c2a616ebc269123
Shivi Vats <shivivats7@gmail.com>
2024-07-17 15:17:05 +0200

    skeleton for battle manager

---
+++ Source/PokemonGame/Private/PBattleManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PBattleManager.h"
+
+// Sets default values
+APBattleManager::APBattleManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APBattleManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APBattleManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
