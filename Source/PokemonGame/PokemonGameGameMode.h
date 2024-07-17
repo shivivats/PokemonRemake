4353b062854f2365722e008b389fcaa7713ce762
Shivi Vats <shivivats7@gmail.com>
2024-07-17 15:16:14 +0200

    Initial commit with third person template

---
+++ Source/PokemonGame/PokemonGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PokemonGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APokemonGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APokemonGameGameMode();
+};
+
+
+
